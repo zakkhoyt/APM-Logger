e9f26c0b7b516ec4d5c91e4023091249477befd6
Zakk Hoyt <vaporwarewolf@gmail.com>
2014-03-24 22:18:55 -0700

    Initial Commit

---
+++ APM LoggerTests/APM_LoggerTests.m
@@ -0,0 +1,34 @@
+//
+//  APM_LoggerTests.m
+//  APM LoggerTests
+//
+//  Created by Zakk Hoyt on 3/24/14.
+//  Copyright (c) 2014 Zakk Hoyt. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface APM_LoggerTests : XCTestCase
+
+@end
+
+@implementation APM_LoggerTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
